aa012d65f3dee9c5132501182280d01f9a7404ba
Jame$ <james@galvanize.com>
2015-10-12 18:32:48 -0600

    Update hello.go

--- hello.go
+++ hello.go
@@ -3,6 +3,9 @@ package hello
 import (
     "fmt"
     "net/http"
+
+    "appengine"
+    "appengine/user"
 )
 
 func init() {
@@ -10,5 +13,17 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "Hello, %v!", u)
 }
